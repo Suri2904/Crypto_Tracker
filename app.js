752da01be5d5b50d71fb3ae4bcd5c2fbe6d0e47f
Suryansh <chande_811963@student.nitw.ac.in>
2022-08-12 20:19:02 +0530

    second commit

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const form = document.querySelector('#searchForm');
 const res = document.querySelector('#tableResult');
 var upd;
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
 
-    e.preventDefault();
-    if(upd){
-        clearTimeout(upd);
-    }
+  e.preventDefault();
+  if (upd) {
+    clearTimeout(upd);
+  }
 
-    const ctype = form.elements.coinType.value;
+  const ctype = form.elements.coinType.value;
 
-    fetchPrice(ctype);
+  fetchPrice(ctype);
 
 });
 
 
-const fetchPrice= async(ctype) =>{
-    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
-    console.log(r.data.coin.price);
-     const price = r.data.coin.price;
-     const volume  = r.data.coin.volume;
-     const change = r.data.coin.priceChange1d;
-     const base = r.data.coin.name;
-     const target = 'INR';
+const fetchPrice = async (ctype) => {
+  const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
+  console.log(r.data.coin.price);
+  const price = r.data.coin.price;
+  const volume = r.data.coin.volume;
+  const change = r.data.coin.priceChange1d;
+  const base = r.data.coin.name;
+  const target = 'INR';
 
 
-     res.innerHTML =`<tr style="background-color:blue; color:white; font-weight:700">
+  res.innerHTML = `<tr style="background-color:blue; color:white; font-weight:700">
      <td>
          Property
      </td>
@@ -50,6 +50,6 @@ const fetchPrice= async(ctype) =>{
      <td>${change}</td>
  </tr>`
 
-    upd = setTimeout(()=>fetchPrice(ctype),10000);
+  upd = setTimeout(() => fetchPrice(ctype), 10000);
 
 }
